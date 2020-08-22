9a2b2a41f6b025ce6fa1fbe6e2b6684c825aea63
Victor <Fedorovich1993@yandex.by>
2020-08-22 18:34:34 +0300

    node-js-intro

---
+++ app.js
@@ -0,0 +1,35 @@
+// NODE SERVER
+// const http = require("http");
+
+// http.createServer((req, res)=>{
+//     res.end('<h1>Server up!</h1>')
+// }).listen(3000, "127.0.0.1",function(){
+//     console.log('listening 3000 port');
+// });
+
+// EXPRESS SERVER
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.get("/register", urlencodedParser, function (request, response) {
+    response.sendFile(__dirname + "/register.html");
+});
+app.post("/register", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.send(`${request.body.userName} - ${request.body.userAge}`);
+});
+  
+app.get("/", function(request, response){
+    response.send("Главная страница");
+});
+
+
+
+
+app.listen(3000, ()=>{
+    console.log('listening 3000 port')
+})
